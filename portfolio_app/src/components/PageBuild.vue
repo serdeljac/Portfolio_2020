133233a30955339196f6e03940f308b9ad706714
serdeljac <hi@stjepanerdeljac.com>
2020-06-14 15:54:22 -0700

    nav and hero build

---
+++ portfolio_app/src/components/PageBuild.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="page-container">
+
+        <div class="navigation" v-if="hideNavigation">
+            <nav>
+                <ul>
+                    <li><a href="#">home</a></li>
+                    <li><a href="#">work</a></li>
+                    <li><a href="#">about</a></li>
+                    <li><a href="#">resume</a></li>
+                    <li><a href="#">contact</a></li>
+                </ul>
+            </nav>
+        </div>
+
+        <div class="hero">
+            <div class="grid grid-column-2">
+                <div class="portfolio">
+                    <!-- Place portfolio image: black and white, svg, animated -->
+                </div>
+                <div>
+                    <header class="grid grid-row-4 header">
+                        <div><h1>Stjepan Josip Erdeljac</h1></div>
+                        <div><h3>web designer and developer</h3></div>
+                        <div><p>small sentence about my work ethic</p></div>
+                        <div><button>Learn More --></button></div>
+                    </header>
+                </div>
+            </div>
+        </div>
+
+        <main>
+            <section class="introduction">
+
+            </section>
+        </main>
+
+        
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                hideNavigation: false,
+            };
+        },
+    };
+</script>
