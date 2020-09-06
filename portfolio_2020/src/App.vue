c80c32ca807f360626073ff902e71b3156c94e72
Stjepan J. Erdeljac <hi@stjepanerdeljac.com>
2020-09-06 00:42:15 -0700

    hero base complete

--- portfolio_2020/src/App.vue
+++ portfolio_2020/src/App.vue
@@ -1,18 +1,64 @@
 <template>
   <div id="app">
-    
-    <Fullpage />
+     <div class="debugValues">
+        <p>Width = {{ winW }}</p>
+        <p>Height = {{ winH }}</p>
+        <p>Display = {{ displayView }}</p>
+    </div>
+    <Hero />
+    <Work />
   </div>
 </template>
 
 <script>
-import Fullpage from './components/Fullpage.vue'
+import Hero from './components/Hero.vue';
+import Work from './components/Work.vue';
+import $ from 'jquery';
 
 export default {
   name: "Assemble",
-  components: { Fullpage }
+  components: { Hero, Work },
+  data() {
+      return {
+          desktopWidth: 1024,
+          tabletWidth: 768,
+          winW: 0,
+          winH: 0,
+          displayView: undefined,
+      }
+  },
+  methods: {
+      getDimensions: function() {
 
+          const browserWidth = $(window).width();
+          const browserHeight = $(window).height();
+          this.winW = browserWidth;
+          this.winH = browserHeight;
+          
+          switch(true) {
+              
+              case browserWidth >= this.desktopWidth:
+                  this.displayView = "desktop";
+                  break;
+              case browserWidth >= this.tabletWidth:
+                  this.displayView = "tablet";
+                  break;
+              case browserWidth >= 0:
+                  this.displayView = "mobile";
+                  break;
+          }
+      }
+  },
+  mounted() {
+      this.getDimensions();
+      window.addEventListener('resize', this.getDimensions);
+      // window.addEventListener('scroll', this.handleScroll);
+      // window.addEventListener('wheel', this.scrollWindow);
+      // window.addEventListener('mousemove', this.mouseMove);
+  },
 }
+
+
 </script>
 
 <style lang="scss">
---
+++ portfolio_2020/src/components/Hero.vue
@@ -0,0 +1,33 @@
+<template>
+    <section class="hero">
+        <div class="hero__wrapper">
+            <div class="hero__header">
+                <header>
+                    <h1>
+                        This is <span>Stjepan Erdeljac</span>. <br />A <span>front-end developer and designer</span> from Vancouver.
+                    </h1>
+
+                    <h3>
+                        He's a <span>wordpress developer, photographer,</span> and a <span>goal-oriented</span> individual. When not coding, he spends his time at the gym, gardening, and researching inspirational UI trends and styles.
+                    </h3>
+                </header>
+                <div class="call_to_action">
+                    <button class="btn view_work">View my Work</button>
+                    <button class="btn contact_me">Contact me</button>
+                </div>
+            </div>
+            <div class="hero__background">
+                <div class="img-wrapper">
+                    <img src="../assets/profile-img_1.jpg" alt="Stjepan Erdeljac" />
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+    export default {
+        name: "Hero",
+    }
+</script>
+
