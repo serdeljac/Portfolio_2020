a3b7b7ff0131b0eb20af12e8cd1761f912378654
serdeljac <hi@stjepanerdeljac.com>
2020-09-09 13:43:22 -0700

    card adjust

--- portfolio_2020/src/components/Practice.vue
+++ portfolio_2020/src/components/Practice.vue
@@ -4,9 +4,17 @@
             <div class="card__container">
                 <div class="card">
                     <div class="card__number">1</div>
-                    <div class="card__logo"></div>
-                    <div class="card__title"></div>
-                    <div class="card__description"></div>
+                    <div class="card__foreground">
+                        <div class="card__logo">
+                            <img src="../assets/WordPress-logotype-simplified.png" alt="wordpress" />
+                        </div>
+                        <div class="card__title">
+                            <h3>Create and customize wordpress templates</h3>
+                        </div>
+                        <div class="card__description">
+                            <p>From picking templates to back-end customization of wordpress functions and performance optimization, I can build you a responsive and secure site that’s easy for users to maintain and update</p>
+                        </div>
+                    </div>
                 </div>
             </div>
 
