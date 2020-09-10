aa330b35fde0fa0868e59c0b4d88ae231b42825c
serdeljac <hi@stjepanerdeljac.com>
2020-09-09 17:16:28 -0700

    practice built (not firefox)

--- portfolio_2020/src/components/Practice.vue
+++ portfolio_2020/src/components/Practice.vue
@@ -2,8 +2,12 @@
     <section class="practice">
         <div class="practice__wrapper">
             <div class="card__container">
-                <div class="card">
-                    <div class="card__number">1</div>
+                <div class="card card-1">
+
+                    <div class="card__background">
+                        <p>1</p>
+                    </div>
+
                     <div class="card__foreground">
                         <div class="card__logo">
                             <img src="../assets/WordPress-logotype-simplified.png" alt="wordpress" />
@@ -16,6 +20,46 @@
                         </div>
                     </div>
                 </div>
+
+                <div class="card card-2">
+
+                    <div class="card__background">
+                        <p>2</p>
+                    </div>
+
+                    <div class="card__foreground">
+                        <div class="card__logo">
+                            <img src="../assets/WordPress-logotype-simplified.png" alt="wordpress" />
+                        </div>
+                        <div class="card__title">
+                            <h3>Adaptive to various technologies</h3>
+                        </div>
+                        <div class="card__description">
+                            <p>Though I primarily work with wordpress and use Vue.JS for building web applications, there’s no hesitation when being asked to use or learn other web technologies to get the project done right.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card card-3">
+
+                    <div class="card__background">
+                        <p>3</p>
+                    </div>
+
+                    <div class="card__foreground">
+                        <div class="card__logo">
+                            <img src="../assets/WordPress-logotype-simplified.png" alt="wordpress" />
+                        </div>
+                        <div class="card__title">
+                            <h3>Communication, planning, and teamwork</h3>
+                        </div>
+                        <div class="card__description">
+                            <p>It’s always a major factor to stay in touch with client and other project developers when working on a project. With a wide range of tools like Slack, Discord, or simply E-mail, there’s no reason not to keep you informed on the latest info.</p>
+                        </div>
+                    </div>
+                </div>
+                
+                
             </div>
 
         </div>
