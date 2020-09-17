11c6cc2f05a56be2b395a5fc5b2172e8c616b45c
Stjepan J. Erdeljac <hi@stjepanerdeljac.com>
2020-09-16 23:36:07 -0700

    added site img and itterations

--- portfolio_2020/src/components/Work.vue
+++ portfolio_2020/src/components/Work.vue
@@ -5,17 +5,15 @@
         </div>
 
         <div class="work__list">
-            <!-- 
-                Append Link
-                Append img as background-image
-             -->
             <div class="site" v-for="site in sites" :key="site.id">
-                <a href="">
-                    <div class="site__title">
+                <a v-bind:href="site.href">
+                    <div class="site__background parallax" v-bind:style="{ 'background-image': 'url(' + site.img + ')' }"></div>
+
+                    <!-- <div class="site__title">
                         <h3>{{ site.name }}</h3>
                         <span>{{ site.date }}</span>
-                    </div>
-                    <div class="site__background parallax"></div>
+                    </div> -->
+                    
                 </a>
             </div>
 
@@ -24,28 +22,30 @@
 </template>
 
 <script>
+    import natalieMiles from "../assets/natalie-miles.jpg";
+
     const myWebsites = [
         {
             id: 1,
             name: 'Natalie Miles',
             href: 'https://natalie-miles.com/',
-            img: '../img/natalie-miles.jpg',
+            img: natalieMiles,
             date: '2019',
         },
-        {
-            id: 2,
-            name: 'Beautiful Brides Philly',
-            href: 'https://beautifulbridesphilly.com/',
-            img: '../img/beautiful-brides-philly.jpg',
-            date: '2019',
-        },
-        {
-            id: 3,
-            name: 'Geoff Lee Mortgage',
-            href: 'https://www.geoffleemortgage.com/',
-            img: '../img/geoff-lee-mortgage.jpg',
-            date: '2018',
-        },
+        // {
+        //     id: 2,
+        //     name: 'Beautiful Brides Philly',
+        //     href: 'https://beautifulbridesphilly.com/',
+        //     img: '../img/beautiful-brides-philly.jpg',
+        //     date: '2019',
+        // },
+        // {
+        //     id: 3,
+        //     name: 'Geoff Lee Mortgage',
+        //     href: 'https://www.geoffleemortgage.com/',
+        //     img: '../img/geoff-lee-mortgage.jpg',
+        //     date: '2018',
+        // },
         // {
         //     id: 4,
         //     name: 'Garage Door Repair - Scranton',
@@ -53,13 +53,13 @@
         //     img: '../img/gdr-scranton.jpg',
         //     date: '2019',
         // },
-        {
-            id: 5,
-            name: 'Ly & Associates',
-            href: 'https://lyandassociatesfg.com/',
-            img: '../img/ly-and-associates.jpg',
-            date: '2019',
-        },
+        // {
+        //     id: 5,
+        //     name: 'Ly & Associates',
+        //     href: 'https://lyandassociatesfg.com/',
+        //     img: '../img/ly-and-associates.jpg',
+        //     date: '2019',
+        // },
         // {
         //     id: 6,
         //     name: 'Spencer Soares',
@@ -67,13 +67,13 @@
         //     img: '../img/spencer-soares.jpg',
         //     date: '2017',
         // },
-        {
-            id: 7,
-            name: 'Easy NOA',
-            href: 'https://www.easynoa.ca/',
-            img: '../img/easy-noa.jpg',
-            date: '2018',
-        },
+        // {
+        //     id: 7,
+        //     name: 'Easy NOA',
+        //     href: 'https://www.easynoa.ca/',
+        //     img: '../img/easy-noa.jpg',
+        //     date: '2018',
+        // },
     ];
 
 
@@ -82,6 +82,7 @@
         data() {
             return {
                 sites: myWebsites,
+                natalieMiles
             }
         },
     }
