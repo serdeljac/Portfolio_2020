b7a13c16f2c5af76d99a45f3f4f8f545420d59fb
serdeljac <hi@stjepanerdeljac.com>
2020-10-26 16:15:14 -0700

    preview build

--- portfolio_2020/src/components/Site_preview.vue
+++ portfolio_2020/src/components/Site_preview.vue
@@ -5,6 +5,21 @@
             
         </div>
 
+        <div class="preview__content">
+            <div class="display"></div>
+            <div class="details">
+                <div class="close"></div>
+                <div class="title"></div>
+                <div class="description"></div>
+                <div class="site_link"></div>
+                <div class="navigation">
+                    <div class="arrow left">Left</div>
+                    <span></span>
+                    <div class="arrow right">Right</div>
+                </div>
+            </div>
+        </div>
+
     </div>
 </template>
 
