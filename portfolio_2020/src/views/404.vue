b268456f7ffd9a42b69e8aabbf075bf5e2722adc
serdeljac <hi@stjepanerdeljac.com>
2020-12-15 21:35:27 -0800

    404 error

--- portfolio_2020/src/views/404.vue
+++ portfolio_2020/src/views/404.vue
@@ -1,15 +1,42 @@
 <template>
-    <div>
-404 Error
+    <div class="error">
+        <div class="error__wrapper">
+            <div class="title">
+                <h2>Cannot find page</h2>
+                <h3>Error: 404</h3>
+            </div>
+            <div class="content">
+                <p>
+                    Something went wrong for the following reasons:
+                </p>
+                <ol>
+                    <li>A link that was clicked cannot find the correct component</li>
+                    <li>An invalid url was entered</li>
+                </ol>
+                <hr />
+                <p class="return">
+                    <a href="/" class="clickable">Click here</a> to return to site
+                </p>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+    import $ from 'jquery';
+    
     export default {
         name: 'NotFound',
+         mounted() {
+            $('.cursor').removeClass('active');
+            $('.clickable').hover(
+                function() {
+                    $('.cursor').addClass('active');
+                },
+                function() {
+                    $('.cursor').removeClass('active');
+                }
+                );
+        }
     }
 </script>
-
-<style lang="scss" scoped>
-
-</style>
